4e45cc397846c3a689ddaecc3a71279075b664fd
Rishabh Verma <verma.rishabh924@gmail.com>
2025-06-22 20:32:00 +0530

    fixing UI for mobile screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     return (
       <div className="w-full py-10">
         <Container>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-2 px-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
             {[...Array(8)].map((_, idx) => (
               <SkeletonCard key={idx} />
             ))}
@@ -42,7 +42,7 @@ const Home = () => {
   return (
     <div className="w-full py-10">
       <Container>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 px-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
           {posts.map((post) => (
             <PostCard key={post.$id} {...post} />
           ))}
